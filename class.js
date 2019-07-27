6195b1283eefb1a087c04c7289e54b87210e9fd8
Engrsun <hyginussunny7@gmail.com>
2019-07-27 15:05:17 +0100

    Reverse a String

--- class.js
+++ class.js
@@ -1,10 +1,14 @@
-
-function factorial(num){
-    let fac = 1;
-    for(let i=num; i >= 1; i--){
-        fac *= i
+// To Convert Celsius to Fahrenheit
+function convertToF(celsius) {
+    let fahrenheit = (celsius * 9/5) + 32;
+    console.log(fahrenheit);
     }
- console.log(fac);
-}
-factorial(4);
+    convertToF(30);
 
+    // Reverse a String
+    function reverseString(str) {
+        let res = str.split('').reverse().join('');
+        console.log(res);
+
+    }
+        reverseString("engineer");
