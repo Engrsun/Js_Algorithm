e5f98a88a9a2d0c1ca934ba1e54ce1c4bb19925f
Engrsun <hyginussunny7@gmail.com>
2019-07-30 10:59:30 +0100

    Fibonancci function

--- class.js
+++ class.js
@@ -12,3 +12,40 @@ function convertToF(celsius) {
 
     }
         reverseString("engineer");
+
+        // Factorialize a Number
+
+        function factorialize(num) {
+            let fac = 1;
+            for(let n = 1; n <= num; n++){
+                fac *=n;
+            }
+            console.log(fac);
+        }
+            factorialize(4);
+
+            // Find the Longest Word in a String AND Return the number
+
+            function findLongestWordLength(str){
+                let newArr = str.split(" ");
+                let longstr = 0;
+                for(let i = 0; i < newArr.length; i++){
+                    if(newArr[i].length > longstr){
+                        longstr = newArr[i].length;
+                    }
+                }
+                console.log(longstr);
+            }
+
+            findLongestWordLength("The quick brown fox jumped over the lazy dog");
+
+            
+        function fib(n) {
+            if (n <= 1) {
+              return n;
+            } else {
+               return  fib(n - 1) + fib(n - 2);
+              
+            }
+          }
+            console.log(fib(10));
