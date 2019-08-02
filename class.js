510bc756a8fa938abe748835a407e64fd08d991c
Engrsun <hyginussunny7@gmail.com>
2019-08-02 15:05:39 +0100

    More js challenge solutions

--- class.js
+++ class.js
@@ -61,10 +61,77 @@ function convertToF(celsius) {
                          } 
                         } console.log(res); 
                     } getIndexToIns([40, 60], 50);
-                
-                    function reverseNum(num){
-                        num = num + "";
-                        return num.split("").reverse().join("");
-                        
-                      }
-                      console.log(reverseNum(1234));
+               
+                    
+function reverseNum(num){
+    num = num + "";
+    return num.split("").reverse().join("");
+    
+    }
+    console.log(reverseNum(1234));
+                      
+
+    function palindrome(str){
+    let str1 = str.toLowerCase();
+    let str2 = str1.split('').reverse().join('');
+    if(str1 == str2){
+        return true;
+    }else{
+        return false;
+    }
+    }
+    console.log(palindrome('madam'));
+
+    //avaScript function that returns a passed string with letters in alphabetical order
+    
+    function alphabeticalOrder(str){
+        return str.split('').sort().join('');
+    }
+
+    console.log(alphabeticalOrder('sunday'));
+
+        //Write a JavaScript function that accepts a string as a parameter and converts the first letter of each word of the string in upper case.
+
+        function strUp(str){
+            let arr = str.split(' ');
+            let newArr = [];
+             for(let i = 0; i < arr.length; i++){
+              newArr.push(arr[i].charAt(0).toUpperCase()+arr[i].slice(1));
+             }
+             return newArr.join(' ');
+             
+           }
+           console.log(strUp('converts the first letter'));
+
+
+        //Find the longest word within a string
+           function longest_word(str){
+               arr = str.split(' ');
+               newArr = arr[0];
+               for(let i =0; i<arr.length; i++){
+                   if(newArr.length < arr[i].length){
+                    newArr = arr[i];
+                   }  
+               }
+               return newArr;
+           }
+           console.log(longest_word('Web Development Tutorial'));
+
+           //Counts the number of vowels within a string
+
+           function numVowels(str) {
+            // let newStr = str.replace(/\s/g,"").match(/[a||e||i||o||u]/gi)
+            // return newStr.length
+            return str.match(/[aeiou]/gi).length;
+
+            }
+            console.log(numVowels("Web development tutorial"));
+
+            function isPrime(num) {
+                for(let i = 2; i < num; i++)
+                  if(num % i === 0) return false;
+                return num > 1;
+              }
+              console.log(isPrime(50));
+
+               
